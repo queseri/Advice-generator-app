54a9668cbaf3b33af9a20c4abf233864000be36d
Chamunorwa Mutezva <ckmutezva@gmail.com>
2022-03-19 17:21:27 +0200

    feat: using the blockquote to improve accessibility by using semantic element

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ async function generate() {
     const fetchAdvice = await fetch("https://api.adviceslip.com/advice", {cache: "no-cache"})
     const response = await fetchAdvice.json()
     mainTitle.innerHTML = `Advice # ${response.slip.id}`
-    advice.innerHTML = response.slip.advice
+    advice.innerHTML = `"${response.slip.advice}"`
     gsap.from(".main-title", {duration: 1, x: 100});
     gsap.from(".advice", {duration: 1, x: -50});
     console.log(response.slip)
